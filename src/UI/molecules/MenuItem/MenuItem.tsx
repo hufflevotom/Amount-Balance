bb6f8b9c82c0469d3b7876558a9559f7cc55637a
Bruno Breña Ramírez <ten.crypto@outlook.com>
2023-05-20 01:46:22 -0500

    feat: menu item molecule

--- src/UI/molecules/MenuItem/MenuItem.tsx
+++ src/UI/molecules/MenuItem/MenuItem.tsx
@@ -1,5 +1,4 @@
-import { Icon } from "../../atoms/Icon";
-import { Title } from "../../atoms/Title";
+import { Paragraph, IconOutlined, Icon } from "@UI/atoms";
 
 export type MenuItemProps = {
   active?: boolean;
@@ -9,11 +8,22 @@ export type MenuItemProps = {
 
 const MenuItem: React.FC<MenuItemProps> = (props) => {
   return (
-    <div>
-      <Icon type={props.icon} size="lg" colored={props.active} />
-      <Title type="h7" color={props.active ? "primary" : "secondary"}>
-        {props.label}
-      </Title>
+    <div
+      className={`w-16 h-16 flex flex-row justify-center items-center ${
+        !props.active && "hover:cursor-pointer"
+      }`}
+    >
+      {props.active && (
+        <hr className="w-1 h-10 bg-purple-600 relative left-0 rounded-full border-purple-600" />
+      )}
+      <div className="flex flex-col w-full justify-center items-center">
+        {props.active ? (
+          <Icon type={props.icon} size="md" colored />
+        ) : (
+          <IconOutlined type={props.icon} size="md" />
+        )}
+        {!props.active && <Paragraph type="details">{props.label}</Paragraph>}
+      </div>
     </div>
   );
 };
--- src/app/layout/Header/Header.tsx
+++ src/app/layout/Header/Header.tsx
@@ -1,9 +1,14 @@
-import React from "react";
+import MenuItem from "../../../UI/molecules/MenuItem/MenuItem";
 
 export type HeaderProps = {};
 
 const Header: React.FC<HeaderProps> = () => {
-  return <div className="text-white">Header</div>;
+  return (
+    <div className="text-white">
+      <MenuItem icon="home" active label="abc" key={1} />
+      <MenuItem icon="home" label="Home" key={2} />
+    </div>
+  );
 };
 
 export default Header;
